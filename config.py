e125caf6f249a85437986bdca2cdb0211cfd235f
wurongfeng1995 <wurongfeng@nd.com.cn>
2024-09-28 23:22:29 +0800

    add local integrate

--- config.py
+++ config.py
@@ -17,5 +17,13 @@ CONFIG = {
         "app/src/main/java/com/nd/smartcan/appfactory/generate/G_app_service.java",
         "app/src/main/java/com/nd/sdp/conf/impl/ServiceConfigImpl.java",
         # 可以根据需要添加更多文件路径
+    ],
+
+    "local_integrate_lib": [
+        "com.nd.xst:llp-userinfo:",
+        "com.nd.sdp.android:llp-x-user-android:",
+        "com.nd.sdp.android:llp-x-cloud-assemble-android:",
+        "com.nd.sdp.android:xst-coursehour:",
+        "com.nd.sdp.android:llp-xst-course:",
     ]
 }
