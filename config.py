19808a8f78019e7080aa27864cfd914e1be6e97c
wurongfeng <wurongfeng@nd.com.cn>
2024-09-29 14:44:56 +0800

    change target project path

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # 配置参数
 CONFIG = {
     # 替换为实际的目标项目路径
-    "target_project_path": os.path.expanduser("/Users/wurf/Code/llp/llp_9_27"),
+    "target_project_path": os.path.expanduser("/Users/nd/Code/LLP-x-cloud/llp_9_27"),
     # 在这里可以添加更多的配置参数
 
     # 环境，可以是 staging 或 pre
